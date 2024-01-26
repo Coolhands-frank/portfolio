e899bc5fd66eaedb07f10028772ee81839d20b06
Elendu Franklin <elendufranklin66@gmail.com>
2024-01-27 00:32:23 +0100

    added project

--- src/projectdata.jsx
+++ src/projectdata.jsx
@@ -8,27 +8,34 @@ export default [
     },
     {
         id: 2,
+        title: "Movie Watchlist",
+        description: "A movie watchlist web application which calls to the OMDB API with the title searched for, display results and adds selected movie to watchlist.",
+        coverImg: "demonstration-767983_1920.jpg",
+        url: "https://coolhands-movie-watchlist.onrender.com/"
+    },
+    {
+        id: 3,
         title: "Restaurant Ordering App",
         description: "A Mock restaurant Ordering app that allows users order items ",
         coverImg: "Screenshot 2023-10-16 210818.png",
         url: "https://resturant-ordering-app.onrender.com"
     },
     {
-        id: 3,
+        id: 4,
         title: "Champions Endorsement",
         description: "A Javascript powered app that allows users send endorsement messages to each other",
         coverImg: "10473117.png",
         url: "https://coolhands-champions.onrender.com/"
     },
     {
-        id: 4,
+        id: 5,
         title: "Password Generator",
         description: "This app allow users to input desired lenght of password and generates 4 random passwords of given length",
         coverImg: "Screenshot 2023-09-19 235646.png",
         url: "https://password-generator-ox0i.onrender.com/"
     },
     {
-        id: 5,
+        id: 6,
         title: "JavaScript Documentation clone",
         description: "A page clone of the JavaScript documentation webpage",
         coverImg: "1_YQgaKfVzK-YpxyT3NYqJAg.png",
@@ -36,7 +43,7 @@ export default [
 
     },
     {
-        id: 6,
+        id: 7,
         title: "Form input page",
         description: "A simply designed and responsive simulation of a registration/enrollment form webpage.",
         coverImg: "Registration-Form-in-HTML-@2x.png",
